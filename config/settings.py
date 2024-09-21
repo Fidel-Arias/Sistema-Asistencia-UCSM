63a267783441a854b962d1a6b09a0fcc996c5dc9
Fidel-Arias <fr06arias16@gmail.com>
2024-09-21 22:49:33 +0000

    configuracion

--- config/settings.py
+++ config/settings.py
@@ -24,11 +24,11 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-leba%ve-o$&mt82ntcxb(lttm7i%#8-tp=zjkx@ut=_2t25zb#'
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = 'django-insecure-leba%ve-o$&mt82ntcxb(lttm7i%#8-tp=zjkx@ut=_2t25zb#'
+ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  'sistemaasistenciaucsm.es',
@@ -40,9 +40,9 @@ CSRF_TRUSTED_ORIGINS = ['https://sistemaasistenciaucsm.es',
 DOMAIN_URL = 'https://sistemaasistenciaucsm.es'
 
 # CONFIGURACION SSL
-# SECURE_SSL_REDIRECT = True  # Redirige automáticamente a HTTPS
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True  # Redirige automáticamente a HTTPS
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 
 # Application definition
